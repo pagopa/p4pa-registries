d8e5d2bcee8da8f81d819ccd88c08caf96831c4e
dan-ntt <danbota1@emeal.nttdata.com>
2025-05-14 09:50:43 +0200

    feat: P4ADEV-2795 created debt position and installment entities and repositories (#3)
    Co-authored-by: Dan Bota <dan.bota@nttdata.com>

---
+++ src/main/java/it/gov/pagopa/pu/registry/repository/DebtPositionRegistryRepository.java
@@ -0,0 +1,9 @@
+package it.gov.pagopa.pu.registry.repository;
+
+import it.gov.pagopa.pu.registry.model.DebtPositionRegistry;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "debt-position-registries")
+public interface DebtPositionRegistryRepository extends MongoRepository<DebtPositionRegistry, String> {
+}
