721c60963ead538fbd175d1e16b15970fc67907f
Dan Bota <dan.bota@nttdata.com>
2025-05-19 09:12:01 +0200

    feat(P4ADEV-2804): implemented custom queries for installment and debt position registry - find all debt position registries by debt position id - find all installment registries by debt position id and nav - find all installment registries by debt position id

--- src/main/java/it/gov/pagopa/pu/registry/repository/InstallmentRegistryRepository.java
+++ src/main/java/it/gov/pagopa/pu/registry/repository/InstallmentRegistryRepository.java
@@ -4,6 +4,10 @@ import it.gov.pagopa.pu.registry.model.InstallmentRegistry;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "installment-registries")
 public interface InstallmentRegistryRepository extends MongoRepository<InstallmentRegistry, String> {
+  List<InstallmentRegistry> findAllByDebtPositionIdAndNav(Long debtPositionId, String nav);
+  List<InstallmentRegistry> findAllByDebtPositionId(Long debtPositionId);
 }
