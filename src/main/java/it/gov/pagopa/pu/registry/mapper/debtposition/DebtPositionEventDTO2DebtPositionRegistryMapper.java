64ed6e383bc05bcba2e636cb27ded96e3fff6ca8
Dan Bota <dan.bota@nttdata.com>
2025-05-19 11:05:50 +0200

    fix(P4ADEV-2796): removed any null checks on the mapper for debt positions and installments

--- src/main/java/it/gov/pagopa/pu/registry/mapper/debtposition/DebtPositionEventDTO2DebtPositionRegistryMapper.java
+++ src/main/java/it/gov/pagopa/pu/registry/mapper/debtposition/DebtPositionEventDTO2DebtPositionRegistryMapper.java
@@ -5,22 +5,11 @@ import it.gov.pagopa.pu.registry.model.DebtPositionRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
-
 @Slf4j
 @Service
 public class DebtPositionEventDTO2DebtPositionRegistryMapper {
 
   public DebtPositionRegistry map(DebtPositionEventDTO dto) {
-    Objects.requireNonNull(dto.getEventId(), "eventId cannot be null");
-    Objects.requireNonNull(dto.getEventType(), "eventType cannot be null");
-    Objects.requireNonNull(dto.getTraceId(), "traceId cannot be null");
-    Objects.requireNonNull(dto.getEventDateTime(), "eventDateTime cannot be null");
-    Objects.requireNonNull(dto.getPayload(), "payload cannot be null");
-    Objects.requireNonNull(dto.getPayload().getDebtPositionId(), "payload.debtPositionId cannot be null");
-    Objects.requireNonNull(dto.getPayload().getUpdateOperatorExternalId(), "payload.updateOperatorExternalId cannot be null");
-    Objects.requireNonNull(dto.getPayload().getOrganizationId(), "payload.organizationId cannot be null");
-
     return DebtPositionRegistry.builder()
       .eventId(dto.getEventId())
       .eventType(dto.getEventType())
--- src/main/java/it/gov/pagopa/pu/registry/mapper/installment/DebtPositionEventDTO2InstallmentRegistryMapper.java
+++ src/main/java/it/gov/pagopa/pu/registry/mapper/installment/DebtPositionEventDTO2InstallmentRegistryMapper.java
@@ -10,55 +10,30 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class DebtPositionEventDTO2InstallmentRegistryMapper {
 
   public List<InstallmentRegistry> map(DebtPositionEventDTO dto) {
-    Objects.requireNonNull(dto.getEventId(), "eventId cannot be null");
-    Objects.requireNonNull(dto.getEventType(), "eventType cannot be null");
-    Objects.requireNonNull(dto.getTraceId(), "traceId cannot be null");
-    Objects.requireNonNull(dto.getEventDateTime(), "eventDateTime cannot be null");
-    Objects.requireNonNull(dto.getPayload(), "payload cannot be null");
-    Objects.requireNonNull(dto.getPayload().getDebtPositionId(), "payload.debtPositionId cannot be null");
-    Objects.requireNonNull(dto.getPayload().getUpdateOperatorExternalId(), "payload.updateOperatorExternalId cannot be null");
-    Objects.requireNonNull(dto.getPayload().getOrganizationId(), "payload.organizationId cannot be null");
-    Objects.requireNonNull(dto.getPayload().getPaymentOptions(), "payload.paymentOptions cannot be null");
+    if (dto.getPayload().getPaymentOptions() == null) return List.of();
 
-    List<InstallmentRegistry> installmentRegistries = new ArrayList<>();
-
-    for (PaymentOptionDTO paymentOptionDTO : dto.getPayload().getPaymentOptions()) {
-      if (paymentOptionDTO.getInstallments() == null) continue;
-
-      for (InstallmentDTO installmentDTO : paymentOptionDTO.getInstallments()) {
-        if (installmentDTO.getNav() == null) {
-          log.warn("InstallmentDTO nav is null, skipping this installment");
-          continue;
-        }
-
-        if (installmentDTO.getUpdateOperatorExternalId() == null) {
-          log.warn("InstallmentDTO updateOperatorExternalId is null, skipping this installment");
-          continue;
-        }
-
-        InstallmentRegistry installmentRegistry = InstallmentRegistry.builder()
-          .eventId(dto.getEventId() + "." + installmentDTO.getNav())
-          .eventType(dto.getEventType())
-          .traceId(dto.getTraceId())
-          .eventDateTime(dto.getEventDateTime())
-          .eventDescription(dto.getEventDescription())
-          .operatorExternalUserId(installmentDTO.getUpdateOperatorExternalId())
-          .debtPositionId(dto.getPayload().getDebtPositionId())
-          .organizationId(dto.getPayload().getOrganizationId())
-          .nav(installmentDTO.getNav())
-          .build();
-
-        installmentRegistries.add(installmentRegistry);
-      }
-    }
-
-    return installmentRegistries;
+    return dto.getPayload().getPaymentOptions().stream()
+      .filter(paymentOptionDTO -> paymentOptionDTO.getInstallments() != null)
+      .flatMap(paymentOptionDTO -> paymentOptionDTO.getInstallments().stream())
+      .map(installmentDTO -> InstallmentRegistry.builder()
+        .eventId(dto.getEventId() + "." + installmentDTO.getNav())
+        .eventType(dto.getEventType())
+        .traceId(dto.getTraceId())
+        .eventDateTime(dto.getEventDateTime())
+        .eventDescription(dto.getEventDescription())
+        .operatorExternalUserId(installmentDTO.getUpdateOperatorExternalId())
+        .debtPositionId(dto.getPayload().getDebtPositionId())
+        .organizationId(dto.getPayload().getOrganizationId())
+        .nav(installmentDTO.getNav())
+        .build())
+      .collect(Collectors.toList());
   }
 
 }
