b104ee2e77302b53826d537fb18178bcf992a0be
Dan Bota <dan.bota@nttdata.com>
2025-05-28 14:29:52 +0200

    feat(P4ADEV-2802): implemented timeline service and connected with consumer

---
+++ src/main/java/it/gov/pagopa/pu/registry/service/TimelineService.java
@@ -0,0 +1,29 @@
+package it.gov.pagopa.pu.registry.service;
+
+import it.gov.pagopa.pu.registry.event.payments.dto.PaymentEventDTO;
+import it.gov.pagopa.pu.registry.service.debtposition.DebtPositionRegistryService;
+import it.gov.pagopa.pu.registry.service.installment.InstallmentRegistryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TimelineService {
+
+  private final DebtPositionRegistryService debtPositionRegistryService;
+  private final InstallmentRegistryService installmentRegistryService;
+
+  public void consumePaymentEvent(PaymentEventDTO<?> event) {
+    if (event == null) {
+      log.warn("Received null payment event, skipping processing.");
+      return;
+    }
+
+    log.info("Processing payment event: {}", event);
+    debtPositionRegistryService.consumePaymentEvent(event);
+    installmentRegistryService.consumePaymentEvent(event);
+  }
+
+}
