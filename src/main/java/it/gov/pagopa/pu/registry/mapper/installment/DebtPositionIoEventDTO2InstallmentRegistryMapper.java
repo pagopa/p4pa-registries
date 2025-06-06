bdbcb788189cbe174fe7b72473d8aaf0c1ea8325
dan-ntt <danbota1@emeal.nttdata.com>
2025-06-06 12:00:55 +0200

    feat: P4ADEV-3021 implemented event description and adapted tests (#16)
    Co-authored-by: Dan Bota <dan.bota@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/registry/mapper/installment/DebtPositionIoEventDTO2InstallmentRegistryMapper.java
+++ src/main/java/it/gov/pagopa/pu/registry/mapper/installment/DebtPositionIoEventDTO2InstallmentRegistryMapper.java
@@ -1,12 +1,16 @@
 package it.gov.pagopa.pu.registry.mapper.installment;
 
 import it.gov.pagopa.pu.registry.event.payments.dto.DebtPositionIoEventDTO;
+import it.gov.pagopa.pu.registry.event.payments.dto.DebtPositionIoNotificationDTO;
 import it.gov.pagopa.pu.registry.model.InstallmentRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -22,12 +26,21 @@ public class DebtPositionIoEventDTO2InstallmentRegistryMapper {
         .eventType(dto.getEventType())
         .traceId(dto.getTraceId())
         .eventDateTime(dto.getEventDateTime())
-        .eventDescription(dto.getEventDescription())
+        .eventDescription(getEventDescription(dto, ioMessageDTO))
         .debtPositionId(dto.getPayload().getDebtPositionId())
         .organizationId(dto.getPayload().getOrganizationId())
         .nav(ioMessageDTO.getNav())
         .build())
       .toList();
+  }
+
+  private String getEventDescription(DebtPositionIoEventDTO dto, DebtPositionIoNotificationDTO.IoMessage message) {
+    return Stream.of(
+        dto.getEventDescription(),
+        Optional.ofNullable(message.getNotificationId()).map(notificationId -> "ioNotificationId: " + notificationId).orElse(null)
+      )
+      .filter(StringUtils::hasText)
+      .collect(Collectors.joining("; "));
   }
 
 }
