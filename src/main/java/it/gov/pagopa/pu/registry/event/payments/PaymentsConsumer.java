ea23a49a305da4f6089dbdd111654210e17fd96b
Dan Bota <dan.bota@nttdata.com>
2025-06-10 17:49:46 +0200

    feat(P4ADEV-3140): rolled back temporary code

--- src/main/java/it/gov/pagopa/pu/registry/event/payments/PaymentsConsumer.java
+++ src/main/java/it/gov/pagopa/pu/registry/event/payments/PaymentsConsumer.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.pu.registry.event.payments;
 
 import it.gov.pagopa.pu.registry.event.payments.dto.PaymentEventDTO;
 import it.gov.pagopa.pu.registry.service.TimelineService;
-import it.gov.pagopa.pu.workflowhub.dto.generated.PaymentEventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,14 +18,6 @@ public class PaymentsConsumer implements Consumer<PaymentEventDTO<?>> {
 
   @Override
   public void accept(PaymentEventDTO paymentEventDTO) {
-    if (
-      paymentEventDTO.getEventType() != PaymentEventType.IO_NOTIFIED
-      && paymentEventDTO.getEventType() != PaymentEventType.SEND_NOTIFICATION_CREATED
-      && paymentEventDTO.getEventType() != PaymentEventType.SEND_NOTIFICATION_DATE
-      && paymentEventDTO.getEventType() != PaymentEventType.SEND_NOTIFICATION_ERROR
-    ) {
-      return;
-    }
     try {
       timelineService.consumePaymentEvent(paymentEventDTO);
     } catch (Exception exception) {
