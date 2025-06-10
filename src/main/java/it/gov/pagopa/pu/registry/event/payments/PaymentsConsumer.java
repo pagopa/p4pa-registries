5a343b604832000f4e57046f72c7c6b7ca4083e4
Dan Bota <dan.bota@nttdata.com>
2025-06-10 17:47:06 +0200

    feat(P4ADEV-3140): implemented filter by IUD code for installment creation

--- src/main/java/it/gov/pagopa/pu/registry/event/payments/PaymentsConsumer.java
+++ src/main/java/it/gov/pagopa/pu/registry/event/payments/PaymentsConsumer.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.pu.registry.event.payments;
 
 import it.gov.pagopa.pu.registry.event.payments.dto.PaymentEventDTO;
 import it.gov.pagopa.pu.registry.service.TimelineService;
+import it.gov.pagopa.pu.workflowhub.dto.generated.PaymentEventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,14 @@ public class PaymentsConsumer implements Consumer<PaymentEventDTO<?>> {
 
   @Override
   public void accept(PaymentEventDTO paymentEventDTO) {
+    if (
+      paymentEventDTO.getEventType() != PaymentEventType.IO_NOTIFIED
+      && paymentEventDTO.getEventType() != PaymentEventType.SEND_NOTIFICATION_CREATED
+      && paymentEventDTO.getEventType() != PaymentEventType.SEND_NOTIFICATION_DATE
+      && paymentEventDTO.getEventType() != PaymentEventType.SEND_NOTIFICATION_ERROR
+    ) {
+      return;
+    }
     try {
       timelineService.consumePaymentEvent(paymentEventDTO);
     } catch (Exception exception) {
--- src/main/java/it/gov/pagopa/pu/registry/mapper/installment/DebtPositionEventDTO2InstallmentRegistryMapper.java
+++ src/main/java/it/gov/pagopa/pu/registry/mapper/installment/DebtPositionEventDTO2InstallmentRegistryMapper.java
@@ -2,9 +2,11 @@ package it.gov.pagopa.pu.registry.mapper.installment;
 
 import it.gov.pagopa.pu.registry.event.payments.dto.DebtPositionEventDTO;
 import it.gov.pagopa.pu.registry.model.InstallmentRegistry;
+import it.gov.pagopa.pu.registry.utils.ExtractionUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class DebtPositionEventDTO2InstallmentRegistryMapper {
@@ -12,9 +14,12 @@ public class DebtPositionEventDTO2InstallmentRegistryMapper {
   public List<InstallmentRegistry> map(DebtPositionEventDTO dto) {
     if (dto.getPayload().getPaymentOptions() == null) return List.of();
 
+    Set<String> iudIds = ExtractionUtils.extractIudIdsFromDescription(dto.getEventDescription());
+
     return dto.getPayload().getPaymentOptions().stream()
       .filter(paymentOptionDTO -> paymentOptionDTO.getInstallments() != null)
       .flatMap(paymentOptionDTO -> paymentOptionDTO.getInstallments().stream())
+      .filter(installmentDTO -> iudIds.isEmpty() || iudIds.contains(installmentDTO.getIud()))
       .map(installmentDTO -> InstallmentRegistry.builder()
         .eventId(dto.getEventId() + "." + installmentDTO.getNav())
         .eventType(dto.getEventType())
