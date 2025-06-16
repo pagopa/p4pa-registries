8b4db809cf41291533b8ebeb3ab0191c45a830e1
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-06-16 17:42:31 +0200

    P4ADEV-3067 fix pr

--- src/main/java/it/gov/pagopa/pu/registry/model/PagopaRegistry.java
+++ src/main/java/it/gov/pagopa/pu/registry/model/PagopaRegistry.java
@@ -1,10 +1,5 @@
 package it.gov.pagopa.pu.registry.model;
 
-import it.gov.pagopa.pu.registry.enums.EventCategory;
-import it.gov.pagopa.pu.registry.enums.EventSubType;
-import it.gov.pagopa.pu.registry.enums.Outcome;
-import it.gov.pagopa.pu.registry.enums.RegistryType;
-import it.gov.pagopa.pu.workflowhub.dto.generated.PaymentEventType;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -23,13 +18,10 @@ public class PagopaRegistry implements Serializable {
 
   @Id
   private String registryId;
-  private PaymentEventType eventType;
-  private OffsetDateTime eventDateTime;
-  private String eventDescription;
-  private String operatorExternalUserId;
+  private String registryOrigin;
+  private String registryType;
+  private OffsetDateTime dateTime;
   private String traceId;
-
-  private RegistryType registryType;
   private String brokerStationId;
   private String orgFiscalCode;
   private String iuv;
@@ -38,10 +30,11 @@ public class PagopaRegistry implements Serializable {
   private String pspId;
   private String pspChannelId;
   private String paymentMethod;
-  private EventCategory eventCategory;
-  private EventSubType eventSubType;
+  private String eventCategory;
+  private String eventType;
+  private String eventSubType;
   private String requestorId;
   private String grantorId;
-  private Outcome outcome;
-  private String body;
+  private String outcome;
+  private byte[] body;
 }
--- src/main/java/it/gov/pagopa/pu/registry/model/SilRegistry.java
+++ src/main/java/it/gov/pagopa/pu/registry/model/SilRegistry.java
@@ -1,9 +1,5 @@
 package it.gov.pagopa.pu.registry.model;
 
-import it.gov.pagopa.pu.registry.enums.EventSubType;
-import it.gov.pagopa.pu.registry.enums.Outcome;
-import it.gov.pagopa.pu.registry.enums.RegistryType;
-import it.gov.pagopa.pu.workflowhub.dto.generated.PaymentEventType;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -22,21 +18,18 @@ public class SilRegistry implements Serializable {
 
   @Id
   private String registryId;
-  private PaymentEventType eventType;
-  private OffsetDateTime eventDateTime;
-  private String eventDescription;
-  private String operatorExternalUserId;
-  private String traceId;
-
   private String registryOrigin;
-  private RegistryType registryType;
+  private String registryType;
+  private OffsetDateTime dateTime;
+  private String traceId;
   private String brokerFiscalCode;
   private String orgFiscalCode;
   private String iuv;
   private String nav;
-  private EventSubType eventSubType;
+  private String eventType;
+  private String eventSubType;
   private String requestorId;
   private String grantorId;
-  private Outcome outcome;
-  private String body;
+  private String outcome;
+  private byte[] body;
 }
