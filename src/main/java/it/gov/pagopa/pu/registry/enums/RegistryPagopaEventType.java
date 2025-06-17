1fea0a9b710e245b143529309087d4c509df2b2d
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-06-17 16:41:50 +0200

    P4ADEV-3067 fix pr

--- src/main/java/it/gov/pagopa/pu/registry/enums/RegistryPagopaEventType.java
+++ src/main/java/it/gov/pagopa/pu/registry/enums/RegistryPagopaEventType.java
@@ -6,19 +6,13 @@ import lombok.Getter;
 @SuppressWarnings("java:S115") // Suppressing constant naming warning: this is required to match with the api name
 public enum RegistryPagopaEventType {
 
-  paVerifyPaymentNotice(true),
-  paGetPaymentV2(true),
-  paSendRTV2(true),
-  newDebtPosition(false),
-  createPosition(false),
-  updatePosition(false),
-  deletePosition(false),
-  fetchPaymentReporting(false);
-
-  private final boolean exposedByPU;
-
-  RegistryPagopaEventType(boolean exposedByPU) {
-    this.exposedByPU = exposedByPU;
-  }
+  paVerifyPaymentNotice,
+  paGetPaymentV2,
+  paSendRTV2,
+  newDebtPosition,
+  createPosition,
+  updatePosition,
+  deletePosition,
+  fetchPaymentReporting;
 
 }
--- src/main/java/it/gov/pagopa/pu/registry/enums/RegistrySilEventType.java
+++ src/main/java/it/gov/pagopa/pu/registry/enums/RegistrySilEventType.java
@@ -6,33 +6,26 @@ import lombok.Getter;
 @SuppressWarnings("java:S115") // Suppressing constant naming warning: this is required to match with the api name
 public enum RegistrySilEventType {
 
-  //API "opzione 1" backward compatible with MyPay - MyPivot
-  paaSILAutorizzaImportFlusso(true),
-  paaSILChiediStatoImportFlusso(true),
-  paaSILImportaDovuto(true),
-  paaSILPrenotaExportFlusso(true),
-  paaSILPrenotaExportFlussoIncrementaleConRicevuta(true),
-  paaSILChiediStatoExportFlusso(true),
-  paaSILInviaDovuti(true),
-  paaSILVerificaAvviso(true),
-  paaSILChiediPagati(true),
-  paaSILChiediPagatiConRicevuta(true),
-  paaSILInviaCarrelloDovuti(true),
-  paaSILChiediEsitoCarrelloDovuti(true),
-  pivotSILAutorizzaImportFlusso(true),
-  pivotSILChiediStatoImportFlusso(true),
-  pivotSILAutorizzaImportFlussoTesoreria(true),
-  pivotSILChiediStatoImportFlussoTesoreria(true),
-  pivotSILPrenotaExportFlussoRiconciliazione(true),
-  pivotSILChiediStatoExportFlussoRiconciliazione(true),
-  pivotSILChiediAccertamento(true),
-  attualizzazioneImporti(false),
-  notificaPagamento(false);
-
-  private final boolean exposedByPU;
-
-  RegistrySilEventType(boolean exposedByPU) {
-    this.exposedByPU = exposedByPU;
-  }
+  paaSILAutorizzaImportFlusso,
+  paaSILChiediStatoImportFlusso,
+  paaSILImportaDovuto,
+  paaSILPrenotaExportFlusso,
+  paaSILPrenotaExportFlussoIncrementaleConRicevuta,
+  paaSILChiediStatoExportFlusso,
+  paaSILInviaDovuti,
+  paaSILVerificaAvviso,
+  paaSILChiediPagati,
+  paaSILChiediPagatiConRicevuta,
+  paaSILInviaCarrelloDovuti,
+  paaSILChiediEsitoCarrelloDovuti,
+  pivotSILAutorizzaImportFlusso,
+  pivotSILChiediStatoImportFlusso,
+  pivotSILAutorizzaImportFlussoTesoreria,
+  pivotSILChiediStatoImportFlussoTesoreria,
+  pivotSILPrenotaExportFlussoRiconciliazione,
+  pivotSILChiediStatoExportFlussoRiconciliazione,
+  pivotSILChiediAccertamento,
+  attualizzazioneImporti,
+  notificaPagamento;
 
 }
