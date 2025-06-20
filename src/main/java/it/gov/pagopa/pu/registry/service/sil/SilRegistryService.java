4df5a3415a26d716ef74d81b9f1e60b2a9341796
dan-ntt <danbota1@emeal.nttdata.com>
2025-06-20 18:45:18 +0200

    feat: P4ADEV-3069 aligned developed services with main branch (#24)
    Co-authored-by: Dan Bota <dan.bota@nttdata.com>

---
+++ src/main/java/it/gov/pagopa/pu/registry/service/sil/SilRegistryService.java
@@ -0,0 +1,30 @@
+package it.gov.pagopa.pu.registry.service.sil;
+
+import it.gov.pagopa.pu.registry.dto.RegistryEventSilDTO;
+import it.gov.pagopa.pu.registry.mapper.pagopa.RegistryEventSilDTO2SilRegistryMapper;
+import it.gov.pagopa.pu.registry.model.SilRegistry;
+import it.gov.pagopa.pu.registry.repository.SilRegistryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SilRegistryService {
+
+  private final SilRegistryRepository silRegistryRepository;
+  private final RegistryEventSilDTO2SilRegistryMapper registryEventSilDTO2SilRegistryMapper;
+
+  @Transactional
+  public void consumePaymentEvent(RegistryEventSilDTO event) {
+    List<SilRegistry> registry = registryEventSilDTO2SilRegistryMapper.map(event);
+    if (CollectionUtils.isEmpty(registry)) return;
+    silRegistryRepository.saveAll(registry);
+  }
+
+}
