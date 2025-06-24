f48c2b1ede582819a6a454c05abca1f6ef9c0ac5
Dan Bota <dan.bota@nttdata.com>
2025-06-24 16:13:34 +0200

    test: pushed tests and corrected them post changes

--- src/test/java/it/gov/pagopa/pu/registry/service/pagopa/PagoPaRegistryServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/registry/service/pagopa/PagoPaRegistryServiceTest.java
@@ -1,6 +1,11 @@
 package it.gov.pagopa.pu.registry.service.pagopa;
 
 import it.gov.pagopa.pu.registry.dto.RegistryEventPagoPaDTO;
+import it.gov.pagopa.pu.registry.dto.generated.PagoPaRegistryDTO;
+import it.gov.pagopa.pu.registry.enums.RegistryEventCategory;
+import it.gov.pagopa.pu.registry.enums.RegistryEventSubType;
+import it.gov.pagopa.pu.registry.enums.RegistryOutcome;
+import it.gov.pagopa.pu.registry.enums.RegistryPagoPaEventType;
 import it.gov.pagopa.pu.registry.mapper.pagopa.RegistryEventPagoPaDTO2PagoPaRegistryMapper;
 import it.gov.pagopa.pu.registry.model.PagoPaRegistry;
 import it.gov.pagopa.pu.registry.repository.PagoPaRegistryRepository;
@@ -12,8 +17,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
+import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class PagoPaRegistryServiceTest {
@@ -44,15 +58,91 @@ class PagoPaRegistryServiceTest {
       new PagoPaRegistry()
     );
 
-    Mockito.when(mapper.map(Mockito.any(RegistryEventPagoPaDTO.class))).thenReturn(pagopaRegistries);
+    Mockito.when(mapper.map(any(RegistryEventPagoPaDTO.class))).thenReturn(pagopaRegistries);
 
     // When
     this.service.consumePaymentEvent(Mockito.mock(RegistryEventPagoPaDTO.class));
 
     // Then
     Mockito.verify(mapper, Mockito.times(1))
-      .map(Mockito.any(RegistryEventPagoPaDTO.class));
+      .map(any(RegistryEventPagoPaDTO.class));
     Mockito.verify(repository, Mockito.times(1))
-      .saveAll(Mockito.any());
+      .saveAll(any());
+  }
+
+  @Test
+  void shouldReturnRegistryDTOWhenRegistryExists() {
+    // Given
+    PagoPaRegistry testEntity = getMockedPagoPaRegistry(UUID.randomUUID().toString());
+    testEntity.setBodyCiphered("test".getBytes(StandardCharsets.UTF_8));
+
+    Mockito.when(repository.findById(testEntity.getRegistryId()))
+      .thenReturn(Optional.of(testEntity));
+    Mockito.when(dataCipherService.decrypt(testEntity.getBodyCiphered()))
+      .thenReturn("test");
+
+    // When
+    PagoPaRegistryDTO result = service.getPagoPaRegistry(testEntity.getRegistryId());
+
+    // Then
+    assertThat(result).isNotNull();
+    assertThat(result.getRegistryId()).isEqualTo(testEntity.getRegistryId());
+    assertThat(result.getDateTime()).isEqualTo(testEntity.getDateTime());
+    assertThat(result.getTraceId()).isEqualTo(testEntity.getTraceId());
+    assertThat(result.getBrokerStationId()).isEqualTo(testEntity.getBrokerStationId());
+    assertThat(result.getOrgFiscalCode()).isEqualTo(testEntity.getOrgFiscalCode());
+    assertThat(result.getIuv()).isEqualTo(testEntity.getIuv());
+    assertThat(result.getNav()).isEqualTo(testEntity.getNav());
+    assertThat(result.getCcp()).isEqualTo(testEntity.getCcp());
+    assertThat(result.getPspId()).isEqualTo(testEntity.getPspId());
+    assertThat(result.getPspChannelId()).isEqualTo(testEntity.getPspChannelId());
+    assertThat(result.getPaymentMethod()).isEqualTo(testEntity.getPaymentMethod());
+    assertThat(result.getEventCategory()).isEqualTo(testEntity.getEventCategory());
+    assertThat(result.getEventType()).isEqualTo(testEntity.getEventType());
+    assertThat(result.getEventSubType()).isEqualTo(testEntity.getEventSubType());
+    assertThat(result.getRequestorId()).isEqualTo(testEntity.getRequestorId());
+    assertThat(result.getGrantorId()).isEqualTo(testEntity.getGrantorId());
+    assertThat(result.getOutcome()).isEqualTo(testEntity.getOutcome());
+    assertThat(result.getBody()).isEqualTo("test");
+
+    Mockito.verify(repository, Mockito.times(1)).findById(testEntity.getRegistryId());
+    Mockito.verify(dataCipherService, Mockito.times(1)).decrypt(testEntity.getBodyCiphered());
+  }
+
+  @Test
+  void shouldThrowWhenRegistryDoesNotExists() {
+    // Given
+    Mockito.when(repository.findById("id123"))
+      .thenReturn(Optional.empty());
+
+    // Then
+    assertThrows(ResourceNotFoundException.class, () -> {
+      service.getPagoPaRegistry("id123");
+    });
+    Mockito.verify(repository, Mockito.times(1)).findById("id123");
+    Mockito.verify(dataCipherService, Mockito.never()).decrypt(any());
+  }
+
+  private PagoPaRegistry getMockedPagoPaRegistry(String id) {
+    return PagoPaRegistry.builder()
+      .registryId(id)
+      .dateTime(OffsetDateTime.now())
+      .traceId("trace123")
+      .brokerStationId("station123")
+      .orgFiscalCode("fiscalCode123")
+      .iuv("iuv123")
+      .nav("nav123")
+      .ccp("ccp123")
+      .pspId("pspId123")
+      .pspChannelId("pspChannelId123")
+      .paymentMethod("paymentMethod123")
+      .eventCategory(RegistryEventCategory.INTERFACCIA)
+      .eventType(RegistryPagoPaEventType.paSendRTV2)
+      .eventSubType(RegistryEventSubType.REQ)
+      .requestorId("requestorId123")
+      .grantorId("grantorId123")
+      .outcome(RegistryOutcome.OK)
+      .bodyCiphered(null)
+      .build();
   }
 }
--- src/test/java/it/gov/pagopa/pu/registry/service/sil/SilRegistryServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/registry/service/sil/SilRegistryServiceTest.java
@@ -1,6 +1,10 @@
 package it.gov.pagopa.pu.registry.service.sil;
 
 import it.gov.pagopa.pu.registry.dto.RegistryEventSilDTO;
+import it.gov.pagopa.pu.registry.dto.generated.SilRegistryDTO;
+import it.gov.pagopa.pu.registry.enums.RegistryEventSubType;
+import it.gov.pagopa.pu.registry.enums.RegistryOutcome;
+import it.gov.pagopa.pu.registry.enums.RegistrySilEventType;
 import it.gov.pagopa.pu.registry.mapper.pagopa.RegistryEventSilDTO2SilRegistryMapper;
 import it.gov.pagopa.pu.registry.model.SilRegistry;
 import it.gov.pagopa.pu.registry.repository.SilRegistryRepository;
@@ -12,8 +16,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
+import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class SilRegistryServiceTest {
@@ -54,5 +67,71 @@ class SilRegistryServiceTest {
       .map(Mockito.any(RegistryEventSilDTO.class));
     Mockito.verify(repository, Mockito.times(1))
       .saveAll(Mockito.any());
+  }
+
+  @Test
+  void shouldReturnRegistryDTOWhenRegistryExists() {
+    // Given
+    SilRegistry testEntity = getMockedSilRegistry(UUID.randomUUID().toString());
+    testEntity.setBodyCiphered("test".getBytes(StandardCharsets.UTF_8));
+
+    Mockito.when(repository.findById(testEntity.getRegistryId()))
+      .thenReturn(Optional.of(testEntity));
+    Mockito.when(dataCipherService.decrypt(testEntity.getBodyCiphered()))
+      .thenReturn("test");
+
+    // When
+    SilRegistryDTO result = service.getSilRegistry(testEntity.getRegistryId());
+
+    // Then
+    assertThat(result).isNotNull();
+    assertThat(result.getRegistryId()).isEqualTo(testEntity.getRegistryId());
+    assertThat(result.getDateTime()).isEqualTo(testEntity.getDateTime());
+    assertThat(result.getTraceId()).isEqualTo(testEntity.getTraceId());
+    assertThat(result.getBrokerFiscalCode()).isEqualTo(testEntity.getBrokerFiscalCode());
+    assertThat(result.getOrgFiscalCode()).isEqualTo(testEntity.getOrgFiscalCode());
+    assertThat(result.getIuv()).isEqualTo(testEntity.getIuv());
+    assertThat(result.getNav()).isEqualTo(testEntity.getNav());
+    assertThat(result.getEventType()).isEqualTo(testEntity.getEventType());
+    assertThat(result.getEventSubType()).isEqualTo(testEntity.getEventSubType());
+    assertThat(result.getRequestorId()).isEqualTo(testEntity.getRequestorId());
+    assertThat(result.getGrantorId()).isEqualTo(testEntity.getGrantorId());
+    assertThat(result.getOutcome()).isEqualTo(testEntity.getOutcome());
+    assertThat(result.getBody()).isEqualTo("test");
+
+    Mockito.verify(repository, Mockito.times(1)).findById(testEntity.getRegistryId());
+    Mockito.verify(dataCipherService, Mockito.times(1)).decrypt(testEntity.getBodyCiphered());
+  }
+
+  @Test
+  void shouldThrowWhenRegistryDoesNotExists() {
+    // Given
+    Mockito.when(repository.findById("id123"))
+      .thenReturn(Optional.empty());
+
+    // Then
+    assertThrows(ResourceNotFoundException.class, () -> {
+      service.getSilRegistry("id123");
+    });
+    Mockito.verify(repository, Mockito.times(1)).findById("id123");
+    Mockito.verify(dataCipherService, Mockito.never()).decrypt(any());
+  }
+
+  private SilRegistry getMockedSilRegistry(String id) {
+    return SilRegistry.builder()
+      .registryId(id)
+      .dateTime(OffsetDateTime.now())
+      .traceId("trace123")
+      .brokerFiscalCode("brokerFiscalCode123")
+      .orgFiscalCode("fiscalCode123")
+      .iuv("iuv123")
+      .nav("nav123")
+      .eventType(RegistrySilEventType.paaSILAutorizzaImportFlusso)
+      .eventSubType(RegistryEventSubType.REQ)
+      .requestorId("requestorId123")
+      .grantorId("grantorId123")
+      .outcome(RegistryOutcome.OK)
+      .bodyCiphered(null)
+      .build();
   }
 }
