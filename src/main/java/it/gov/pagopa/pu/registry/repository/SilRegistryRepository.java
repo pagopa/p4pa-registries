952428e1b7292de54b41bffaad85c964240012f0
ElisKina-dev <elis.kina@nttdata.com>
2025-06-26 17:52:55 +0200

    fix eventType in SilRegistryRepository

--- src/main/java/it/gov/pagopa/pu/registry/repository/SilRegistryRepository.java
+++ src/main/java/it/gov/pagopa/pu/registry/repository/SilRegistryRepository.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.pu.registry.repository;
 
-import it.gov.pagopa.pu.registry.enums.RegistryPagoPaEventType;
+import it.gov.pagopa.pu.registry.enums.RegistrySilEventType;
 import it.gov.pagopa.pu.registry.model.SilRegistry;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,7 +22,7 @@ public interface SilRegistryRepository extends MongoRepository<SilRegistry, Stri
     "        { $or: [{ $expr: { $eq: ['?4', 'null'] }}, { iuv: ?4 }] }" +
     "    ] }", fields = "{bodyCiphered:  0}")
   Page<SilRegistry> searchByFilters(
-    RegistryPagoPaEventType eventType,
+    RegistrySilEventType eventType,
     OffsetDateTime startDate,
     OffsetDateTime endDate,
     String orgFiscalCode,
